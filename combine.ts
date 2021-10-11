c1a9edeff0076580d35e694e9aafc0b32265c43c
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-10-11 21:47:27 +0900

    feat: combine type

---
+++ combine.ts
@@ -0,0 +1,8 @@
+/**
+ * I want to merge (intersection, extends) the T type and the U type. However, there is something in common between the T type and the U type, so they cannot be combined.
+ * So, it is a Util type that first removes and merges common types.
+ * 
+ * T 타입과 U 타입을 합치고(intersection, extends) 싶다. 그러나 T 타입과 U 타입에 공통적인 것이 있어서 합칠 수가 없다.
+ * 그래서 공통적인 타입을 먼저 제거하고 합치는 Util 타입이다.
+ */
+type Combine<T, U> = T & Omit<U, keyof T>
