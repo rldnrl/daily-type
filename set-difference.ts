240f6cf0cd170f84bd60ff12a9a6c627e47c8bbe
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-19 09:27:09 +0900

    feat: define set difference type

---
+++ set-difference.ts
@@ -0,0 +1,10 @@
+/**
+ * 차집합
+ * SetDifference<'1' | '2' | '3', '2' | '3' | '5' | '9'>
+ * 결과: '1'
+ * 
+ * SetDifference<string | number | () => void, Function>
+ * 결과: string | number
+ */
+
+type SetDifference<A, B> = A extends B ? never : A
