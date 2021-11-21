c7979ff894bf332da954f662002f3314170b57f9
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-22 08:11:44 +0900

    feat: define non-function-key type

---
+++ non-function-key.ts
@@ -0,0 +1,18 @@
+/**
+ * type MixedProps = {
+ *   name: string;
+ *   setName: (name: string) => void;
+ *   someKeys?: string;
+ *   someFn?: (...args: any) => any;
+ * };
+ * 
+ * NonFunctionKeys<MixedProps>
+ * 
+ * 결과: "name" | "someKeys"
+ */
+
+type NonUndefined<A> = A extends undefined ? never : A
+
+type NonFunctionKey<T extends object> = {
+  [Key in keyof T] -?: NonUndefined<T[Key]> extends Function ? never : T[Key]
+}[keyof T]
