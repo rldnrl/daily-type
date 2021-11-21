5c0e5a384f7ac8955bd4ad2bbed9ca5b3930c6f8
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-22 08:10:58 +0900

    feat: define function-key type

---
+++ function-key.ts
@@ -0,0 +1,18 @@
+/**
+ * type MixedProps = {
+ *   name: string;
+ *   setName: (name: string) => void;
+ *   someKeys?: string;
+ *   someFn?: (...args: any) => any;
+ * };
+ * 
+ * FunctionKey<MixedProps>
+ * 
+ * 결과: "setName" | "someFn"
+ */
+
+type NonUndefined<A> = A extends undefined ? never : A
+
+type FunctionKey<T extends object> = {
+  [K in keyof T] -?: NonUndefined<T[K]> extends Function ? K : never
+}[keyof T]
