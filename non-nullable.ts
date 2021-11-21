e10bf0ecf23a3735bac84549f29bdea57a6c8ae3
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-21 10:48:09 +0900

    feat: define non nullable type

---
+++ non-nullable.ts
@@ -0,0 +1,10 @@
+/**
+ * Null 아닌 타입
+ * MyNonNullable<string | null | undefined>
+ * 결과: string
+ * 
+ * Null이나 Undefined가 아닌 타입(built-in)
+ * type NonNullable<A> = A extends null | undefined ? never : A
+ */
+
+type MyNonNullable<A> = A extends null ? never : A
