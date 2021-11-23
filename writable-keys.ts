055196bb2dddcc9953d0c5580c98cf928efbf83c
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-24 08:46:11 +0900

    feat: define writable-keys type

---
+++ writable-keys.ts
@@ -0,0 +1,11 @@
+type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B
+
+type MutableKeys<T extends object> = {
+  [P in keyof T]-?: IfEquals<
+    { [Q in P]: T[P] },
+    { -readonly [Q in P]: T[P] },
+    P
+  >
+}[keyof T]
+
+type WritableKeys<T extends object> = MutableKeys<T>
