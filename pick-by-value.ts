364e6a02e84e99bedfa228dae59e564d7e9d6e5d
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-11-27 14:45:41 +0900

    feat: define pick-by-value type

---
+++ pick-by-value.ts
@@ -0,0 +1,35 @@
+/**
+ * T 객체와 일치하는 타입 추출하는 타입
+ * 
+ * type User = {
+ *   firstName: string
+ *   lastName: string
+ *   age?: number 
+ *   email: string
+ * }
+ * 
+ * PickByValue<User, string>
+ * 결과
+ * {
+ *   firstName: string
+ *   lastName: string
+ *   email: string
+ * }
+ * 
+ * type Product = {
+ *   price: number
+ *   discount?: number
+ * }
+ * 
+ * PickByValue<Product, number | undefined>
+ * 결과
+ * {
+ *   price: number;
+ *   discount?: number | undefined;
+ * }
+ */
+
+type PickByValue<T, ValueType> = Pick<
+  T, 
+  { [Key in keyof T]-?: T[Key] extends ValueType ? Key : never }[keyof T]
+>
