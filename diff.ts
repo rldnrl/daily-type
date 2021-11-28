04a79feb25faef89584b725647b75b2182bc957d
Park Kiwook <fender.wook1002@gmail.com>
2021-11-29 08:54:29 +0900

    feat: define diff type

---
+++ diff.ts
@@ -0,0 +1,18 @@
+type SetDifference<A, B> = A extends B ? never : A
+
+/**
+ * Diff
+ * T로부터 U에 있는 공통 프로퍼티를 제거한다.
+ * type Props = { name: string; age: number; visible: boolean };
+ * type DefaultProps = { age: number };
+ *
+ * Diff<Props, DefaultProps>;
+ * {
+ *   name: string;
+ *   visible: boolean;
+ * }
+ */
+type Diff<T, U> = Pick<
+  T,
+  SetDifference<keyof T, keyof U>
+>
