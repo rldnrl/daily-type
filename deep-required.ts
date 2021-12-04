0505be18eb3692563975a1de572ae5a0d5fa7dae
Park Kiwook <fender.wook1002@gmail.com>
2021-12-04 13:50:41 +0900

    feat: add non-undefined

--- deep-required.ts
+++ deep-required.ts
@@ -2,6 +2,29 @@ type NonUndefined<A> = A extends undefined ? never : A
 
 /**
  * 객체 타입의 안에 값들을 필수로 만들어주는 타입
+ * 
+ * type User = {
+ *   firstName?: string
+ *   lastName?: string
+ *   age?: number
+ *   email?: string
+ *   info?: {
+ *     company: string
+ *   }
+ *   techStack?: ["react", "typescript", "functional programming"]
+ * }
+ * 
+ * 결과
+ * {
+ *   firstName: string
+ *   lastName: string
+ *   age: number
+ *   email: string
+ *   info: {
+ *     company: string
+ *   }
+ *   techStack: ["react", "typescript", "functional programming"]
+ * }
  */
 type DeepRequired<T> = T extends (...args: any[]) => any
   ? T
@@ -14,5 +37,5 @@ type DeepRequired<T> = T extends (...args: any[]) => any
 interface _DeepReuiredArray<T> extends Array<DeepRequired<NonUndefined<T>>> {}
 
 type _DeepRequiredObject<T extends object> = {
-  [P in keyof T]-?: DeepRequired<T[P]>
+  [P in keyof T]-?: DeepRequired<NonUndefined<T[P]>>
 }
