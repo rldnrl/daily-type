3118965a4b425ffef2e8540ffa6abc38512a18bd
Park Kiwook <fender.wook1002@gmail.com>
2021-12-09 08:35:17 +0900

    wip: typesafe actions

--- typesafe-actions/type-helpers.ts
+++ typesafe-actions/type-helpers.ts
@@ -1,12 +1,12 @@
 interface Types {}
 
-type TypeConstant = string;
+export type TypeConstant = string;
 
 /**
  * @description Action을 대표하는 타입
  */
 
-type Action<TType extends TypeConstant = TypeConstant> = {
+export type Action<TType extends TypeConstant = TypeConstant> = {
   type: TType
 }
 
@@ -14,7 +14,7 @@ type Action<TType extends TypeConstant = TypeConstant> = {
  * @description ActionCreator를 대표하는 타입
  */
 
-type ActionCreator<TType extends TypeConstant = TypeConstant> = (
+export type ActionCreator<TType extends TypeConstant = TypeConstant> = (
   ...args: any[]
 ) => Action<TType>
 
@@ -22,7 +22,7 @@ type ActionCreator<TType extends TypeConstant = TypeConstant> = (
  * @description Reducer를 대표하는 타입
  */
 
-type Reducer<TState, TAction extends Action> = (
+export type Reducer<TState, TAction extends Action> = (
   state: TState | undefined,
   action: TAction
 ) => TState
@@ -31,6 +31,56 @@ type Reducer<TState, TAction extends Action> = (
  * @description payload 없는 action
  */
 
-type EmptyAction<TType extends TypeConstant> = {
+export type EmptyAction<TType extends TypeConstant> = {
   type: TType
+}
+
+/**
+ * @description Action과 Payload
+ */
+
+export type PayloadAction<TType extends TypeConstant, TPayload> = {
+  type: TType,
+  payload: TPayload
+}
+
+/**
+ * @description Action과 Payload, Meta
+ */
+
+export type PayloadMetaAction<TType extends TypeConstant, TPayload, TMeta> = {
+  type: TType,
+  payload: TPayload,
+  meta: TMeta
+}
+
+/**
+ * @description EmptyAction을 생성하는 Action Creator
+ */
+
+export type EmptyActionCreator<TType extends TypeConstant> = () => EmptyAction<TType>
+
+/**
+ * @description PayloadAction을 생성하는 Action Creator
+ */
+
+export type PayloadActionCreator<TType extends TypeConstant, TPayload> = (
+  payload: TPayload
+) => PayloadAction<TType, TPayload>
+
+/**
+ * @description PayloadMetaAction을 생성하는 Action Creator
+ */
+
+export type MetaPayloadActionCreator<TType extends TypeConstant, TPayload, TMeta> = (
+  payload: TPayload,
+  meta: TMeta
+) => PayloadMetaAction<TType, TPayload, TMeta>
+
+/**
+ * @description Action Creator 인스턴스의 타입 getter를 나타내는 타입
+ */
+
+export interface ActionCreatorTypeMetadata<TType extends TypeConstant> {
+  getType?: () => TType
 }
