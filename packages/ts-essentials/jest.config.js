cb0d7f0d2424d730ba98c3fca3277dd2e1bfc069
rldnrl <devpark1002@gmail.com>
2022-03-31 22:25:40 +0900

    feat: set up file

---
+++ packages/ts-essentials/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'ts-essentials',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/ts-essentials',
+}
